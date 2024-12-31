21c1e0b994adfefcd5c6cb7d13c06cc2fb5a14ed
Vipul Singh <v.ipulsingh592005@gmail.com>
2024-12-31 18:28:23 +0530

    Update app.py
    making the function for the preprocessing of the dataset

--- app.py
+++ app.py
@@ -9,3 +9,9 @@ st.set_page_config("🍕 Sales Analysis")
 # Load the dataset
 st.title("🍕Pizza Sales Analysis🍕")
 df = pd.read_csv(r'pizzas.csv')
+
+# sending the data for the preprocessing
+preprocessed_df = visual.preprocess(df)
+# presenting the dataframe to the user
+st.dataframe(preprocessed_df)
+# making a button to show analysis
